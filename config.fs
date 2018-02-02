fa5027b25fffa782bbe881031cf39b6a2eac6fc3
David Ng <dave@codeaurora.org>
2018-02-02 16:30:02 +0000

    kenzo: Add common AID's to config.fs
    * Drop AID_QCOM_DIAG, AID_RFS and AID_RFS_SHARED

--- config.fs
+++ config.fs
@@ -1,11 +1,5 @@
-[AID_QCOM_DIAG]
-value:2950
-
-[AID_RFS]
-value:2951
-
-[AID_RFS_SHARED]
-value:2952
+[AID_QTI_DIAG]
+value:2901
 
 [/system/vendor/bin/wcnss_filter]
 mode: 0755
@@ -35,4 +29,16 @@ caps: NET_BIND_SERVICE
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
 caps: NET_BIND_SERVICE
