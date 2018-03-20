0eaa8809e4378066d6cc02ea636b0047f8110275
Wight554 <wight554@gmail.com>
2018-03-20 17:56:53 +0000

    rootdir: Dynamically set zram size
    2GB RAM - 1024MB, 3GB RAM - 512MB

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -16,13 +16,32 @@ set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
 echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
 echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
 
+# Dynamic ZRAM
+configure_zram()
+{
+if [ -e /sys/block/zram0/disksize ] ; then
+  swapoff /dev/block/zram0  > /dev/null 2>&1
+  echo 1 > /sys/block/zram0/reset
+  echo $SWAPSIZE > /sys/block/zram0/disksize
+  mkswap /dev/block/zram0
+  swapon /dev/block/zram0  > /dev/null 2>&1
+fi
+}
+
 #Set Low memory killer minfree parameters
 # 64 bit up to 2GB with use 14K, and above 2GB will use 18K
 #
 # Set ALMK parameters (usually above the highest minfree values)
 # 64 bit will have 81K
+#
+# Also dynamically set zram size (512MB for 3GB RAM and 1024MB for 2GB RAM)
 if [ $MemTotal -gt 2097152 ]; then
     echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
+    SWAPSIZE=536870912
 else
     echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
+    SWAPSIZE=1073741824
 fi
+
+# Run ZRAM configure
+configure_zram
