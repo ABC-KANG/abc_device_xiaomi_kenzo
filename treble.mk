18d5a7e61a4ec1c114a2b94cb396004d4baec35f
Wight554 <wight554@gmail.com>
2018-03-28 20:37:23 +0000

    kenzo: Fix up more sepolicy stuff
    * Import some sepolicy from los kenzo tree
    * Fix all seclabels
    * Switch to non-binderized consumerir hal (somehow causes stucks on bootanimation with enforcing)

--- treble.mk
+++ treble.mk
@@ -44,8 +44,7 @@ PRODUCT_PACKAGES += \
 
 # Consumerir
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir@1.0-impl
 
 # Display
 PRODUCT_PACKAGES += \
