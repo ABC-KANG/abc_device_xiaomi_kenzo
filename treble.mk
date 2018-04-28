5f937e4fb48bec48b74ba7ea07ca53986047afcc
Wight554 <wight554@gmail.com>
2018-04-28 23:17:07 +0000

    kenzo: Use QTI GNSS service

--- treble.mk
+++ treble.mk
@@ -72,8 +72,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl\
-    android.hardware.gnss@1.0-impl-qti
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti \
 
 # Keymaster
 PRODUCT_PACKAGES += \
